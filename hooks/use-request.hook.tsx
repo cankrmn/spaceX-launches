9a1186cdc4af18f3ceca166701872a8051fd8d2d
cankrmn <thcnkrmn@outlook.com>
2023-02-18 19:26:59 +0300

    imlpemented useRequest hook

--- hooks/use-request.hook.tsx
+++ hooks/use-request.hook.tsx
@@ -13,9 +13,20 @@ const useRequest = () => {
 	};
 
 	const request = async ({ url, method = "POST", date }: UseRequestArgs) => {
+		const dayStart = new Date(date);
+		dayStart.setUTCHours(0, 0, 0, 0);
+
+		const dayEnd = new Date(date);
+		dayEnd.setUTCHours(23, 59, 59, 999);
+
+		console.log({ dayStart, dayEnd });
+
 		const body = {
 			query: {
-				date_utc: date,
+				date_utc: {
+					$gte: dayStart,
+					$lte: dayEnd,
+				},
 			},
 		};
 
@@ -23,9 +34,9 @@ const useRequest = () => {
 			method,
 			headers: _header,
 			body: JSON.stringify(body),
-		});
+		}).then((res) => res.json());
 
-		return res.json();
+		return res["docs"] ?? [];
 	};
 
 	return request;
